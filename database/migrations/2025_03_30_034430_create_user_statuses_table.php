062f52c36be674420ae5f32fd71c78532daccfb0
rickalfa <rb__samael_@hotmail.es>
2025-03-31 01:44:06 -0300

    se agrego el UserController y el metodo de vaildar credenciales

--- database/migrations/2025_03_30_034430_create_user_statuses_table.php
+++ database/migrations/2025_03_30_034430_create_user_statuses_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
            
          
+           
             $table->unique('user_id');
             $table->timestamps();
         });
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 
+Route::post('/validar-credenciales', [UserController::class, 'validarCredenciales']);
 
